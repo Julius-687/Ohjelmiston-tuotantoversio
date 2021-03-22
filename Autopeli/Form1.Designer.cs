1910c4350fc8a68ce282332e16a0ad821f6533dd
Julius-687 <62982422+Julius-687@users.noreply.github.com>
2021-03-22 13:01:45 +0200

    Add project files.

---
+++ Autopeli/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace Autopeli
+{
+    partial class Autopeli
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Autopeli));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pisteet = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trophy = new System.Windows.Forms.PictureBox();
+            this.pelaaja = new System.Windows.Forms.PictureBox();
+            this.rajahdys = new System.Windows.Forms.PictureBox();
+            this.auto2 = new System.Windows.Forms.PictureBox();
+            this.auto1 = new System.Windows.Forms.PictureBox();
+            this.tie2 = new System.Windows.Forms.PictureBox();
+            this.tie1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trophy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pelaaja)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rajahdys)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.auto2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.auto1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tie2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tie1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.panel1.Controls.Add(this.trophy);
+            this.panel1.Controls.Add(this.pelaaja);
+            this.panel1.Controls.Add(this.rajahdys);
+            this.panel1.Controls.Add(this.auto2);
+            this.panel1.Controls.Add(this.auto1);
+            this.panel1.Controls.Add(this.tie2);
+            this.panel1.Controls.Add(this.tie1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(380, 498);
+            this.panel1.TabIndex = 0;
+            // 
+            // pisteet
+            // 
+            this.pisteet.AutoSize = true;
+            this.pisteet.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pisteet.Location = new System.Drawing.Point(12, 525);
+            this.pisteet.Name = "pisteet";
+            this.pisteet.Size = new System.Drawing.Size(105, 31);
+            this.pisteet.TabIndex = 1;
+            this.pisteet.Tag = "pisteet";
+            this.pisteet.Text = "Pisteet";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(139, 525);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "button1";
+            this.button1.Text = "Aloita";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trophy
+            // 
+            this.trophy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.trophy.Image = ((System.Drawing.Image)(resources.GetObject("trophy.Image")));
+            this.trophy.Location = new System.Drawing.Point(34, 193);
+            this.trophy.Name = "trophy";
+            this.trophy.Size = new System.Drawing.Size(184, 92);
+            this.trophy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.trophy.TabIndex = 2;
+            this.trophy.TabStop = false;
+            // 
+            // pelaaja
+            // 
+            this.pelaaja.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.pelaaja.Image = ((System.Drawing.Image)(resources.GetObject("pelaaja.Image")));
+            this.pelaaja.Location = new System.Drawing.Point(159, 395);
+            this.pelaaja.Name = "pelaaja";
+            this.pelaaja.Size = new System.Drawing.Size(50, 100);
+            this.pelaaja.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pelaaja.TabIndex = 1;
+            this.pelaaja.TabStop = false;
+            // 
+            // rajahdys
+            // 
+            this.rajahdys.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.rajahdys.Image = ((System.Drawing.Image)(resources.GetObject("rajahdys.Image")));
+            this.rajahdys.Location = new System.Drawing.Point(241, 180);
+            this.rajahdys.Name = "rajahdys";
+            this.rajahdys.Size = new System.Drawing.Size(113, 105);
+            this.rajahdys.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rajahdys.TabIndex = 1;
+            this.rajahdys.TabStop = false;
+            this.rajahdys.Tag = "rajahdys";
+            // 
+            // auto2
+            // 
+            this.auto2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.auto2.Image = ((System.Drawing.Image)(resources.GetObject("auto2.Image")));
+            this.auto2.Location = new System.Drawing.Point(274, 36);
+            this.auto2.Name = "auto2";
+            this.auto2.Size = new System.Drawing.Size(50, 100);
+            this.auto2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.auto2.TabIndex = 1;
+            this.auto2.TabStop = false;
+            this.auto2.Tag = "auto2";
+            // 
+            // auto1
+            // 
+            this.auto1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.auto1.Image = ((System.Drawing.Image)(resources.GetObject("auto1.Image")));
+            this.auto1.Location = new System.Drawing.Point(54, 36);
+            this.auto1.Name = "auto1";
+            this.auto1.Size = new System.Drawing.Size(50, 100);
+            this.auto1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.auto1.TabIndex = 1;
+            this.auto1.TabStop = false;
+            this.auto1.Tag = "auto1";
+            // 
+            // tie2
+            // 
+            this.tie2.BackColor = System.Drawing.Color.Black;
+            this.tie2.Image = ((System.Drawing.Image)(resources.GetObject("tie2.Image")));
+            this.tie2.Location = new System.Drawing.Point(0, -499);
+            this.tie2.Name = "tie2";
+            this.tie2.Size = new System.Drawing.Size(381, 499);
+            this.tie2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tie2.TabIndex = 0;
+            this.tie2.TabStop = false;
+            // 
+            // tie1
+            // 
+            this.tie1.Image = ((System.Drawing.Image)(resources.GetObject("tie1.Image")));
+            this.tie1.Location = new System.Drawing.Point(0, 0);
+            this.tie1.Name = "tie1";
+            this.tie1.Size = new System.Drawing.Size(381, 499);
+            this.tie1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tie1.TabIndex = 0;
+            this.tie1.TabStop = false;
+            // 
+            // Autopeli
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 656);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pisteet);
+            this.Controls.Add(this.panel1);
+            this.Name = "Autopeli";
+            this.Text = "Autopeli";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.moveCar);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.stopCar);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trophy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pelaaja)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rajahdys)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.auto2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.auto1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tie2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tie1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pelaaja;
+        private System.Windows.Forms.PictureBox rajahdys;
+        private System.Windows.Forms.PictureBox auto2;
+        private System.Windows.Forms.PictureBox auto1;
+        private System.Windows.Forms.PictureBox tie2;
+        private System.Windows.Forms.PictureBox tie1;
+        private System.Windows.Forms.Label pisteet;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox trophy;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Autopeli/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autopeli
+{
+    public partial class Autopeli : Form
+    {
+
+        int carSpeed = 5;
+        int roadSpeed = 5;
+        bool carLeft;
+        bool carRight;
+        int trafficSpeed = 5;
+        int Score = 0;
+        Random rnd = new Random();
+
+        private void Reset()
+        {
+            trophy.Visible = false;
+
+            button1.Enabled = false; 
+
+            rajahdys.Visible = false; 
+
+            trafficSpeed = 5; 
+
+            roadSpeed = 5; 
+
+            Score = 0; 
+
+            pelaaja.Left = 161;
+            pelaaja.Top = 286;
+
+            carLeft = false;
+            carRight = false;
+
+
+            auto1.Left = 66;
+            auto1.Top = -120;
+
+            auto2.Left = 294;
+            auto2.Top = -185;
+
+
+            tie1.Left = 0;
+            tie1.Top = 105;
+            tie2.Left = 0;
+            tie2.Top = -500;
+
+
+            timer1.Start();
+
+        }
+
+        public Autopeli()
+        {
+            InitializeComponent();
+            Reset();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Score++;
+
+            pisteet.Text = "" + Score;
+
+            tie2.Top += roadSpeed;
+            tie1.Top += roadSpeed;
+
+
+            if (tie2.Top > 499)
+            {
+                tie2.Top = -499;
+            }
+            if (tie1.Top > 499)
+            {
+                tie1.Top = -499;
+            }
+            if (carLeft) { pelaaja.Left -= carSpeed; } 
+            if (carRight) { pelaaja.Left += carSpeed; } 
+
+
+            if (pelaaja.Left < 1)
+            {
+                carLeft = false; 
+            }
+            else if (pelaaja.Left + pelaaja.Width > 380)
+            {
+                carRight = false;
+            }
+
+
+
+            auto1.Top += trafficSpeed;
+            auto1.Top += trafficSpeed;
+
+
+
+            if (pelaaja.Bounds.IntersectsWith(auto1.Bounds) || pelaaja.Bounds.IntersectsWith(auto2.Bounds))
+            {
+                gameOver(); 
+            }
+
+            if (Score > 100 && Score < 500)
+            {
+                trafficSpeed = 6;
+                roadSpeed = 7;
+            }
+
+            else if (Score > 500 && Score < 1000)
+            {
+                trafficSpeed = 7;
+                roadSpeed = 8;
+            }
+
+            else if (Score > 1200)
+            {
+                trafficSpeed = 9;
+                roadSpeed = 10;
+            }
+
+
+        }
+
+        private void moveCar(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && pelaaja.Left > 0)
+            {
+                carLeft = true;
+            }
+
+            if (e.KeyCode == Keys.Right && pelaaja.Left + pelaaja.Width < panel1.Width)
+            {
+                carRight = true;
+            }
+        }
+
+        private void stopCar(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                carLeft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                carRight = false;
+            }
+        }
+
+        private void gameOver()
+        {
+            trophy.Visible = true; 
+
+            timer1.Stop();
+
+            button1.Enabled = true; 
+
+
+            rajahdys.Visible = true; 
+            pelaaja.Controls.Add(rajahdys); 
+            rajahdys.Location = new Point(-8, 5);
+            rajahdys.BackColor = Color.Transparent;
+            rajahdys.BringToFront();
+
+
+            if (Score < 1000)
+            {
+                trophy.Image = Properties.Resources.pronssia;
+
+            }
+
+            if (Score > 2000)
+            {
+                trophy.Image = Properties.Resources.hopeaa;
+            }
+
+
+            if (Score > 3500)
+            {
+                trophy.Image = Properties.Resources.kultaa;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+    }
+}
